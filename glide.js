9ffc122677745b556cc9f53a1c0c95d23b9bda2e
Srirag Hariharan <sriraghariharan108@gmail.com>
2024-03-23 11:50:01 +0530

    Added 'best seller' & 'explore collections' section.

--- glide.js
+++ glide.js
@@ -30,3 +30,25 @@ new Glide('.exclusive-producuts-glide', {
         }
     }
 }).mount();
+
+
+new Glide('.best-sellers-glide', {
+    type: 'carousel',
+    perView: 4,
+    autoplay: 9000,
+    hoverpause: true,
+    animationDuration: 2000,
+    animationTimingFunc: 'ease-in-out',
+
+    pagination: {
+        el: '.hero-section-glide__bullets',
+        clickable: true
+    },
+    breakpoints: {
+        576: {
+            perView: 2
+        }
+    }
+}).mount();
+
+
