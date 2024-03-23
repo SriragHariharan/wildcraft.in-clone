b18b217cc0c3d89743a2b5de6bafbd497a2c654a
Srirag Hariharan <sriraghariharan108@gmail.com>
2024-03-23 09:17:38 +0530

    Added wildcraft exclusive products section.

--- glide.js
+++ glide.js
@@ -11,3 +11,25 @@ new Glide('.hero-section-glide', {
         clickable: true
     }
 }).mount();
+
+new Glide('.exclusive-producuts-glide', {
+    type: 'carousel',
+    perView: 3,
+    autoplay: 9000,
+    hoverpause: true,
+    animationDuration: 2000,
+    animationTimingFunc: 'ease-in-out',
+
+    pagination: {
+        el: '.hero-section-glide__bullets',
+        clickable: true
+    },
+    breakpoints: {
+        768: {
+            perView: 2
+        },
+        576: {
+            perView: 2
+        }
+    }
+}).mount();
